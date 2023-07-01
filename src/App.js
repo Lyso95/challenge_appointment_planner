db8149ead955d98437d5a56055c618b6868d631b
Lyso95 <amos@langensiepen.net>
2023-07-01 23:11:41 +0200

    Finished Contacts -> App.js -> ContactsPage.js -> ContactForm.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,17 +9,33 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+ const [contacts, setContact] = useState([]);
+ const [appointments, setAppointment] = useState([]);
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
+ function addContact(contact){
+  setContact((prev) => {
+    console.log(contact)
+    return [...prev, contact]
+  });
+ };
+ function addAppointment({name, contact, date, time}){
+  setAppointment((prev) => {
+    return[...prev, {name, contact, date, time}]
+  });
+ }
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} addContact={addContact}/> /* Add props to ContactsPage */ }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage 
+                                                    appointments={appointments}
+                                                    addAppointment={addAppointment} 
+                                                  /> /* Add props to AppointmentsPage */ }/>
     </Route>
   ));
   
--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,16 +1,27 @@
 import React from "react";
 
 export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
+    name,
+    setName,
+    phone,
+    setPhone,
+    email,
+    setEmail,
+    handleSubmit
 }) => {
+//TODO: RegEx for Entries!!!
   return (
-    <></>
+    <>
+      <form id="contactForm" name="contactForm" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" defaultValue={name} id="name" setName={e => setName(e.target.value)}/>
+        <label htmlFor="phone">Phone:</label>
+        <input type="tel" name="phone" defaultValue={phone} id="phone" setPhone={e => setPhone(e.target.value)} />
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" defaultValue={email} id="email" setEmail={e => setEmail(e.target.value)} />
+        <input type="submit" id="submit" value="Submit" />
+      </form>
+    </>
   );
 };
 
